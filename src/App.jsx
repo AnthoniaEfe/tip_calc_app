d308c0248b536d7fc1e0cabc12f6ad8be9ca1e8f
AnthoniaEfe <anthoniaefe36@gmail.com>
2025-03-19 12:52:53 +0100

    desktop  view complete

--- src/App.jsx
+++ src/App.jsx
@@ -8,31 +8,32 @@ function App() {
 
   return (
     <>
-    <h1>SPLITTER</h1>
-    <div id="grid-container">
+    <h1></h1>
+    <div id="card-container">
       <form>
-        <label htmlFor="bill-input">Bill 
-        <FontAwesomeIcon icon={faDollarSign} />
+        <label>Bill 
+        <FontAwesomeIcon className="icons" icon={faDollarSign} />
           <input type="number" name="bill-input" id="bill-input" placeholder="0"/>
        </label>
 
-<div className="tip-section"> <label htmlFor="tip" id="tip-label">Select Tip % </label>
-       <button className="tip" name="tip">5%</button>
-       <button className="tip" name="tip">10%</button>
-       <button className="tip" name="tip">15%</button>
-       <button className="tip" name="tip">25%</button>
-       <button className="tip" name="tip">50%</button>
-       
+      <div className="tip-section"> 
+        <label htmlFor="tip">Select Tip % </label>
+        <button className="tip" name="tip">5%</button>
+        <button className="tip" name="tip">10%</button>
+        <button className="tip" name="tip">15%</button>
+        <button className="tip" name="tip">25%</button>
+        <button className="tip" name="tip">50%</button>
         <input type="number" name="tip" placeholder="custom" id="custom-tip"/> 
         </div>
     
-    <label htmlFor="people-num">Number of People
-          <FontAwesomeIcon icon={faUser}  />
+    <label>Number of People
+          <FontAwesomeIcon className="icons" icon={faUser}  />
           <input type="number" name="people-num" id="people-num" placeholder="5"/>
         </label>
       </form>
 
       <div id="tip-container">
+        <div id="tip-calc-container">
         <div id="calc-tip">
           <div className="tip-text">
             <h2>tip amount</h2>
@@ -48,7 +49,8 @@ function App() {
          </div>
           <p className="tip-value">$50.85</p>
         </div>
-      
+      </div>
+
       <button id="reset-btn">RESET</button>
         
       </div>
